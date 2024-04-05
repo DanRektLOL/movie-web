eebdbd03e252deadf52d8a28d6fa8a9e942824e0
DanRektLOL <134997700+DanRektLOL@users.noreply.github.com>
2024-04-05 20:54:41 +0500

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "https://proxydani.netlify.app",
+  VITE_CORS_PROXY_URL: "https://proxydani.netlify.app"
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTNiYjY4ODY2MmQ1MDkxOGQxZDRlMGFiY2VjNzI3YiIsInN1YiI6IjY1ZmVhZmY1Mzc4MDYyMDE2MjM4Yzk3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HxP3yNaSU6NEA3ibaYMABt-J6DhUuaOf6T54SwPeinM",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTNiYjY4ODY2MmQ1MDkxOGQxZDRlMGFiY2VjNzI3YiIsInN1YiI6IjY1ZmVhZmY1Mzc4MDYyMDE2MjM4Yzk3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HxP3yNaSU6NEA3ibaYMABt-J6DhUuaOf6T54SwPeinM"
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
@@ -13,7 +13,7 @@ window.__CONFIG__ = {
   VITE_NORMAL_ROUTER: false,
 
   // The backend URL to communicate with
-  VITE_BACKEND_URL: https://mw-backend.lonelil.ru,
+  VITE_BACKEND_URL: https://mw-backend.lonelil.ru
 
   // A comma separated list of disallowed IDs in the case of a DMCA claim - in the format "series-<id>" and "movie-<id>"
   VITE_DISALLOWED_IDS: "",
